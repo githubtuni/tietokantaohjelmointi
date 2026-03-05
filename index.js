a737d734bf5d0fbd29e2f2ad685d6bade8737499
Alisa <alisa.koskinen@tuni.fi>
2026-03-05 15:23:39 +0200

    paikalliset muutokset

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+import pool from './db.js'
+
+async function test() {
+    try {
+        const result = await pool.query("SELECT * FROM asiakas")
+        console.log(result.rows)
+    } catch (err) {
+        console.error("Virhe:", err)
+    }
+}
+
+
+test()
