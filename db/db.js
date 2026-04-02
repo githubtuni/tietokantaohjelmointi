c1619fb4d50d74a747037bb896f0828c4752b0ee
Alisa <alisa.koskinen@tuni.fi>
2026-04-02 13:19:43 +0300

    r2 varten toteutettu loput funktiot

--- db/db.js
+++ db/db.js
@@ -147,11 +147,84 @@ export async function getTuntityotByLasku(lasku_id) {
 
 export async function getTarvikkeetByLasku(lasku_id) {
   const result = await pool.query(
-    `SELECT t.nimi, lt.maara 
+    `SELECT t.nimi, lt.kpl 
      FROM lasku_tarvike lt
      JOIN tarvike t ON t.tarvike_id = lt.tarvike_id
      WHERE lt.lasku_id = $1`,
     [lasku_id]
   );
   return result.rows;
+}
+
+export async function getLaskuFull(lasku_id) {
+  const result = await pool.query(`
+    SELECT 
+      l.lasku_id,
+      l.tyotyyppi,
+      a.nimi AS asiakas_nimi, a.osoite AS asiakas_osoite,
+      tk.nimi AS kohde_nimi, tk.osoite AS kohde_osoite
+    FROM lasku l
+    JOIN tyokohde tk ON l.tyokohde_id = tk.tyokohde_id
+    JOIN asiakas a ON tk.asiakas_id = a.asiakas_id
+    WHERE l.lasku_id = $1
+  `, [lasku_id]);
+
+  return result.rows[0];
+}
+
+export async function getTuntityotFull(lasku_id) {
+  const result = await pool.query(
+    `SELECT 
+        tt.nimi,
+        tt.hinta,
+        t.tunnit,
+        t.alepros
+     FROM tuntityo t
+     JOIN tyotyyppi tt ON t.tyotyyppi_id = tt.tyotyyppi_id
+     WHERE t.lasku_id = $1`,
+    [lasku_id]
+  );
+  return result.rows;
+}
+
+export async function getTarvikkeetFull(lasku_id) {
+  const result = await pool.query(
+    `SELECT 
+        t.nimi,
+        t.myyntihinta,
+        lt.kpl,
+        lt.alepros
+     FROM lasku_tarvike lt
+     JOIN tarvike t ON t.tarvike_id = lt.tarvike_id
+     WHERE lt.lasku_id = $1`,
+    [lasku_id]
+  );
+  return result.rows;
+}
+
+export function laskeLasku(tuntityot, tarvikkeet) {
+  let tyoSumma = 0;
+  let tarvikeSumma = 0;
+
+  for (let t of tuntityot) {
+    const hinta = t.hinta * t.tunnit;
+     const ale = t.alepros || 0;  
+  const alennus = hinta * (ale / 100);
+    tyoSumma += (hinta - alennus);
+  }
+
+  for (let tar of tarvikkeet) {
+    const hinta = tar.myyntihinta * tar.kpl;
+      const ale = tar.alepros || 0; 
+  const alennus = hinta * (ale / 100);
+
+    tarvikeSumma += (hinta - alennus);
+    
+  }
+  return {
+  tyoSumma,
+  tarvikeSumma,
+  kokonais: tyoSumma + tarvikeSumma,
+  kotitalousvahennys: tyoSumma
+};
 }
