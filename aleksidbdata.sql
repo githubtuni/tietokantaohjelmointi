be7e69b30c096e551be541f8a7d307c5aaaace8c
Aleksi Pellikka <aleksi.pellikka@tuni.fi>
2026-04-16 15:52:55 +0300

    Add part of required esimerkkidata

--- aleksidbdata.sql
+++ aleksidbdata.sql
@@ -96,26 +96,143 @@ CREATE TABLE tuntityo (
     FOREIGN KEY (tyotyyppi_id) REFERENCES tyotyyppi(tyotyyppi_id)
 );
 
-INSERT INTO tyotyyppi (nimi, hinta, hinta_pvm) VALUES ('Suunnittelu', 44.35, '01-01-2026');
-INSERT INTO tyotyyppi (nimi, hinta, hinta_pvm) VALUES ('Työ', 36.29, '01-01-2026');
-INSERT INTO tyotyyppi (nimi, hinta, hinta_pvm) VALUES ('Aputyö', 28.23, '01-01-2026');
+INSERT INTO asiakas (asiakas_id, nimi, osoite) VALUES 
+(1, 'Hosunen Jaska', 'Susimetsä'),
+(2, 'Jokinen Lissu', 'Nurmitie'),
+(3, 'Näsänen Masa', 'Masalantie');
 
-INSERT INTO toimittaja (nimi, osoite) VALUES
-('ABB Oy', 'Niittylänkuja 2, 00320 Helsinki, Finland'),
-('Schneider Electric Finland Oy', 'Keilaranta 1, 02150 Espoo, Finland'),
-('Hager Oy', 'Sammontie 7, 00370 Helsinki, Finland'),
-('Philips Finland Oy', 'Keilaranta 7, 02150 Espoo, Finland'),
-('Siemens Oy', 'Tekniikantie 12, 02150 Espoo, Finland');
+INSERT INTO tyokohde (asiakas_id, nimi, osoite) VALUES
+(1, 'Asunto', 'Susimetsä'),
+(2, 'Asunto', 'Nurmitie'),
+(2, 'Mökki', 'Huitsinneva'),
+(3, 'Mökki', 'Puotonkorpi'),
+(3, 'Asunto', 'Masalantie');
+
+INSERT INTO tyotyyppi (tyotyyppi_id, nimi, hinta, hinta_pvm) VALUES (1, 'Suunnittelu', 44.35, '01-01-2026');
+INSERT INTO tyotyyppi (tyotyyppi_id, nimi, hinta, hinta_pvm) VALUES (2, 'Työ', 36.29, '01-01-2026');
+INSERT INTO tyotyyppi (tyotyyppi_id, nimi, hinta, hinta_pvm) VALUES (3, 'Aputyö', 28.24, '01-01-2026');
+
+INSERT INTO toimittaja (toimittaja_id, nimi, osoite) VALUES
+(1, 'ABB Oy', 'Niittylänkuja 2, 00320 Helsinki, Finland'),
+(2, 'Schneider Electric Finland Oy', 'Keilaranta 1, 02150 Espoo, Finland'),
+(3, 'Hager Oy', 'Sammontie 7, 00370 Helsinki, Finland'),
+(4, 'Philips Finland Oy', 'Keilaranta 7, 02150 Espoo, Finland'),
+(5, 'Siemens Oy', 'Tekniikantie 12, 02150 Espoo, Finland'),
+(6, 'How-data', 'HowTie 1'),
+(7, 'Moponet', 'Moponetinkuja 1'),
+(8, 'Tärsky Pub', 'Tie 1'),
+(9, 'Junk Co', 'Junkintie 5'),
+(10, 'Mopenet', 'Kuja 2');
 
 INSERT INTO tarvike (toimittaja_id, nimi, merkki, alv, yksikko, myyntihinta, sisaanostohinta, varastotilanne)
 VALUES
 (1, 'Johdonsuoja', 'ABB', 24.00, 'kpl', 35.90, 28.50, 50),
 (2, 'Pistorasia', 'Schneider', 24.00, 'kpl', 12.50, 9.80, 120),
-(5, 'Kytkin', 'Hager', 24.00, 'kpl', 7.90, 5.50, 200),
-(3, 'Led-valaisin 12W', 'Philips', 24.00, 'kpl', 29.90, 22.50, 75),
+(3, 'Kytkin', 'Hager', 24.00, 'kpl', 7.90, 5.50, 200),
+(5, 'Led-valaisin 12W', 'Philips', 24.00, 'kpl', 29.90, 22.50, 75),
 (2, 'Kaapeli 3x2.5mm²', 'Nexans', 24.00, 'm', 1.50, 1.10, 1000),
-(4, 'Sulake 16A', 'Siemens', 24.00, 'kpl', 3.50, 2.50, 300),
+(5, 'Sulake 16A', 'Siemens', 24.00, 'kpl', 3.50, 2.50, 300),
 (5, 'Liitäntälaite LED', 'Tridonic', 24.00, 'kpl', 18.90, 14.20, 60),
 (2, 'Kytkinpistorasia combo', 'Schneider', 24.00, 'kpl', 22.50, 17.80, 90),
 (1, 'Kaapelikiinnike 10mm', 'ABB', 24.00, 'kpl', 0.35, 0.20, 500),
-(3, 'Johdinsarja 5m', 'Philips', 24.00, 'kpl', 15.90, 12.50, 80);
+(4, 'Johdinsarja 5m', 'Philips', 24.00, 'kpl', 15.90, 12.50, 80),
+(7, 'Sahkojohto', 'Moponet', 24.00, 'm', 1.25, 1, 500),
+(8, 'Opaskirja', 'Tärsky', 10.00, 'kpl', 10, 8, 150),
+(7, 'Pistorasia', 'Moponet', 24.00, 'kpl', 12.5, 10, 50),
+(9, 'Palohälytin', 'Junk', 24.00, 'kpl', 5, 4, 15);
+
+-- Jokinen Lissu esimerkkilasku nro 4 esimerkkidatasta.
+INSERT INTO lasku (
+  lasku_id,
+  tyokohde_id,
+  lahetys_pvm,
+  maksu_pvm,
+  laskun_tila,
+  tyotyyppi,
+  erapaiva,
+  maksettu,
+  ed_lasku_id,
+  viivastyskorko,
+  laskutuslisa,
+  muistutusnumero
+)
+VALUES (1, 2,'2026-02-01','2026-02-15','Valmis','tuntityö','2026-02-15',true,NULL,0,0,0);
+
+-- Tuntityöt yllä olevaan nro 4 laskuun
+INSERT INTO tuntityo (
+  lasku_id,
+  tyotyyppi_id,
+  paivamaara,
+  tunnit,
+  alepros
+)
+VALUES 
+(1,1,'2026-01-28',3,10),
+(1,2,'2026-01-28',12,0);
+
+-- Tarvikkeet nro 4 yllä olevaan laskuun
+INSERT INTO lasku_tarvike (lasku_id, tarvike_id, kpl, alepros)
+VALUES
+(1, 11, 3, 10),
+(1, 12, 1, 0),
+(1, 13, 1, 20);
+
+-- Jokinen Lissu esimerkkidata lasku nro 8
+INSERT INTO lasku (
+  lasku_id,
+  tyokohde_id,
+  lahetys_pvm,
+  maksu_pvm,
+  laskun_tila,
+  tyotyyppi,
+  erapaiva,
+  maksettu,
+  ed_lasku_id,
+  viivastyskorko,
+  laskutuslisa,
+  muistutusnumero
+)
+VALUES (2, 3,'2026-03-01','2026-03-15','Valmis','urakkatyö','2026-02-15',false,NULL,0,0,0);
+
+-- Lasku nro 8 esimerkkidatasta
+INSERT INTO urakkatyo (lasku_id, sopimuspaiva, aloituspaiva, lopetuspaiva, hinta, tila) VALUES
+(2, '2026-03-01', '2026-03-05', '2026-03-05', 50, null);
+
+-- Lasku nro 8 esimerkkidatasta
+INSERT INTO lasku_tarvike (lasku_id, tarvike_id, kpl, alepros)
+VALUES (2, 14, 2, 0);
+
+-- Näsänen Masa esimerkkilasku nro 9 esimerkkidatasta
+INSERT INTO lasku (
+  lasku_id,
+  tyokohde_id,
+  lahetys_pvm,
+  maksu_pvm,
+  laskun_tila,
+  tyotyyppi,
+  erapaiva,
+  maksettu,
+  ed_lasku_id,
+  viivastyskorko,
+  laskutuslisa,
+  muistutusnumero
+)
+VALUES (3, 5,'2026-03-01',null,'Valmis','tuntityö','2026-03-15',false,NULL,0,0,0);
+
+-- Tuntityöt yllä olevaan laskuun Masalle
+INSERT INTO tuntityo (
+  lasku_id,
+  tyotyyppi_id,
+  paivamaara,
+  tunnit,
+  alepros
+)
+VALUES 
+(3,1,'2026-01-28',3,0),
+(3,2,'2026-01-28',12,0);
+
+-- Tarvikkeet yllä olevaan laskuun Masalle
+INSERT INTO lasku_tarvike (lasku_id, tarvike_id, kpl, alepros)
+VALUES
+(3, 11, 3, 0),
+(3, 13, 1, 0);
